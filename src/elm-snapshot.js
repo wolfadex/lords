f6ee8139eea358c65508767ec506531c87727239
wolfadex <wolfadex@gmail.com>
2020-09-14 08:38:50 -0600

    start on snapshotting model

---
+++ src/elm-snapshot.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+/* find `var $author$project$___$init = function` and conver it into
+ * var $author$project$___$init = function() {
+ *   return snapshotOfModel;
+ * }
+ */
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { Elm } from "./Main.elm";
+import snapshot from "./elm-snapshot";
+
+
+Elm.Main.init();
